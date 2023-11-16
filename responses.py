fda8060c829a105583294f95c259239c8220f445
Muhammad Hassan <124890970+harry7747@users.noreply.github.com>
2023-11-16 05:19:21 +0500

    Initial commit

---
+++ responses.py
@@ -0,0 +1,19 @@
+import random
+
+def get_response(message: str) -> str:
+    p_message = message.lower()
+    
+    if p_message == "hello":
+        return 'Hey there!'
+    
+    if message == 'roll':
+        return str(random.randint(1, 6))
+    
+    if p_message == '!help':
+        return '`This is a help message.`'
+    
+    if p_message == 'Oliver':
+        return 'Go study nigga'
+    
+    return 'I didn\'t understand what you wrote. Try typing "!help".'
+
